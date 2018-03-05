1835f951ad1c231b258591f88ecf25e23ca6b393
jadhavkunal <jadhavkunal111@gmail.com>
2018-03-05 12:07:50 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,9 @@
+x = 34-23 # acoment.
+y = "hello"
+z = 3.45
+if z == 3.45 or y == "hello":
+	x = x+1
+	y = y+"world"
+print x
+print y
+
---
+++ stud.py
@@ -0,0 +1,10 @@
+n = input("enter the no of student:")
+
+student = []
+ 
+for i in range(0,n):
+  roll_no=input("enter the roll no")
+  name=input("enter the name")
+  stud_class=input("enter the class") 
+
+dict={'roll_no':roll_no,'name':name,'class':stud_class}
